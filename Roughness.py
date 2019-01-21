491f87c5f7a18208b302bf9d84322535b69abcb9
paul <paulsouthard1@gmail.com>
2019-01-21 14:53:50 -0600

    Updated Roughness script and development file

---
+++ Roughness.py
@@ -0,0 +1,87 @@
+import numpy as np
+import numpy.ma as ma
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Produce ASCII raster of roughness values a la Casas et al, 2010')
+parser.add_argument('ZRef_File', help='ASCII Raster of D values to calculate Epsilon')
+parser.add_argument('Depth_File', help='ASCII Raster of h values to caclulate Epsilon')
+parser.add_argument('N_File', help='Name for ASCII Raster output of roughness values')
+parser.add_argument('Background_N',help='Background Roughness value of channel, where there is no vegetation')
+args = parser.parse_args()
+
+
+# Define function to load Z ref values in
+def LoadZRef(ZRef_File):
+    # Load array of Zref
+    zref = np.genfromtxt('zref',delimiter = ' ',skip_header = 6)
+    # Remove all negative values and NoData
+    zref[zref < 0] = 0
+    return zref
+
+# Define function to load depth values in
+def LoadDepth(Depth_File):
+    # Load array of depth
+    depth = np.genfromtxt('depth.txt',delimiter = ' ',skip_header = 6)
+    depth[depth < 0] = 0
+    return depth
+
+# Define function to calculate array of Epsilon values
+def Epsilon(zref,depth):
+    newzref = ma.masked_where(zref == 0,zref)
+    epsilon = depth/zref
+    epsilon = ma.masked_where(epsilon <= 0.2,epsilon)
+    epsilon = ma.masked_where(epsilon >= 7,epsilon)
+    return epsilon
+
+# Define fucntion to calculate array of function of alpha and epsilon
+def Func(alpha,epsilon):
+    function = ma.masked_where(epsilon == 0, epsilon)
+    function = 1+(alpha*(1/function)*np.log((np.cosh((1/alpha)-((1/alpha)*function)))/(np.cosh(1/alpha))))
+    return function
+
+# Define function to calculate array of roughness values
+def NCalc(Cu,g,depth,function):
+    n = (depth**(1/6)/((g**(1/2))*Cu*function))
+    return n
+
+# Define function to read in raster ASCII header and create output raster ASCII
+def ReadWriteRaster(inraster,outraster,array):
+    from numpy import savetxt
+    with open(inraster,"r") as file:
+        header = {}
+        for i in range(6):
+            header[i]=file.readline()
+    file.close()
+    with open(outfile,'w') as file:
+        for i in range(6):
+            file.write(header[i])
+    file.close()
+    savetxt('vals.txt',n)
+    datafile = open('vals.txt','r')
+    data = datafile.read()
+    datafile.close()
+    with open(outfile,'a') as file:
+        file.write(data)
+    file.close()
+
+def Main(ZRef_File,Depth_File,N_File,Background_n):
+    # Define constants
+    Cu = 4.5
+    a = 1
+    g = 9.8 # m/s^2
+    # Load in values from rasters as arrays
+    zref = LoadZRef(ZRef_File)
+    depth = LoadDepth(Depth_File)
+    # Produced masked array of roughness
+    eps = Epsilon(zref,depth)
+    func = Func(a,eps)
+    n = NCalc(Cu,g,depth,func)
+    # Fill masked values with default of 0.04
+    Background_N = float(Background_n)
+    n = ma.filled(n,fill_value=Background_n)
+    ReadWriteRaster(ZRef_File,N_File,Background_n)
+
+Main(args.ZRef_File,args.Depth_File,args.N_File,args.Background_N)
+
+
